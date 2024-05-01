f00a7ffd827e3f6df28cd8a63d3b2a182c84b53e
dvlopjs <crissenn99@gmail.com>
2024-05-01 13:24:39 -0300

    remove useServerSession

--- app/utils/useServerSession.ts
+++ app/utils/useServerSession.ts
@@ -1,11 +1,11 @@
 "use server";
 
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+// import { authOptions } from "../api/auth/[...nextauth]/route";
 
 // Custom hook para obtener la sesión del servidor
 export const useServerSession = async () => {
-  const session = await getServerSession(authOptions);
-
-  return session;
+  // const session = await getServerSession(authOptions);
+  return "uwu";
+  // return session;
 };
